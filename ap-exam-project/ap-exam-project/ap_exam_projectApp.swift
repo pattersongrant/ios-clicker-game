0f08fcdae96d0d6b87d44e36f391bbed762e1263
Grant Patterson <grantpatterson@Grants-MacBook-Pro.local>
2023-05-12 11:24:15 -0400

    first commit

---
+++ ap-exam-project/ap-exam-project/ap_exam_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ap_exam_projectApp.swift
+//  ap-exam-project
+//
+//  Created by Grant Patterson on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ap_exam_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
