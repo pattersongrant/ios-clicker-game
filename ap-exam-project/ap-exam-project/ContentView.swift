eadd1e4f02267abd019a8859cfc9a94538057b05
Grant Patterson <grantpatterson@Grants-MacBook-Pro.local>
2023-05-17 12:15:03 -0400

    superstartthing

--- ap-exam-project/ap-exam-project/ContentView.swift
+++ ap-exam-project/ap-exam-project/ContentView.swift
@@ -7,10 +7,16 @@
 
 import SwiftUI
 var isHappy = true
+var allOff = false
 struct ContentView: View {
     @State var clicked = false
+    //@State var update = 0
     var body: some View {
-        if (clicked==false){
+        if allOff{
+            SuperStart()
+            //update+=1
+        }
+        if (clicked==false&&allOff==false){
             VStack {
                 Image("javaphoto")
                     .imageScale(.large)
@@ -34,7 +40,8 @@ struct ContentView: View {
 struct NewView: View {
     @State var clicked2 = false
     var body: some View {
-        if(clicked2==false){
+
+        if(clicked2==false&&allOff==false){
             Text("Welcome to my App")
         }
         Button(action:{
@@ -42,14 +49,14 @@ struct NewView: View {
             isHappy.toggle()
             
         }) {
-            if isHappy{
+            if isHappy && allOff==false{
                 Image("smiley")
                     .resizable()
                     .frame(width: 100.0, height: 100.0)
                     .imageScale(.large)
                     .foregroundColor(.accentColor)
             }
-            else{
+            if !isHappy && allOff==false {
                 Image("wat")
                     .resizable()
                     .frame(width: 100.0, height: 100.0)
@@ -65,11 +72,23 @@ struct NewView: View {
     }
 }
 struct NewView2: View {
-    
+    @State var finalClick = false
+    @State var realAppStart = false
     var body: some View {
+        
         Text("Ouch, why hit me")
+        Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+            finalClick = true
+            allOff=true
+            print("heya testing")
+            
         
-        
+        }
+        if finalClick == true{
+            TesterView()
+            ContentView()
+            
+        }
     }
 }
 
--- ap-exam-project/ap-exam-project/view2.swift
+++ ap-exam-project/ap-exam-project/view2.swift
@@ -7,14 +7,21 @@
 
 import SwiftUI
 
-struct view2: View {
+struct TesterView: View {
     var body: some View {
         Text("Hello, World!")
+    }
+}
+struct SuperStart: View {
+    var body: some View {
+        Text("Started.")
     }
 }
 
 struct view2_Previews: PreviewProvider {
     static var previews: some View {
-        view2()
+        SuperStart()
+        TesterView()
+        
     }
 }
